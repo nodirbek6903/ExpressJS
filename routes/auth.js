266928956c7e126746ec9f1171f6676cb6f29c14
nodirchik6903 <nu2913634@gmail.com>
2023-10-03 19:39:56 +0500

    Login and Register page

--- routes/auth.js
+++ routes/auth.js
@@ -13,5 +13,13 @@ router.get("/register",(req,res) => {
         isRegister:true
     })
 })
+router.post("/login", (req,res) => {
+    console.log(req.body);
+    res.redirect("/")
+})
+router.post("/register", (req,res) => {
+    console.log(req.body);
+    res.redirect("/")
+})
 
 export default router
