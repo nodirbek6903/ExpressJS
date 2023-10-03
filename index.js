edb0f15e3d926cd3be68c464c6fea25946c8eefc
nodirchik6903 <nu2913634@gmail.com>
2023-10-03 19:56:17 +0500

    public folder

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const hbs = create({
 app.engine("hbs",hbs.engine)
 app.set("view engine","hbs")
 app.set("views","./views")
+ 
 app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
