ea9ff821a706d1c6e2ad701981abf91e0a3d8237
nodirchik6903 <nu2913634@gmail.com>
2023-10-06 20:20:36 +0500

    Login handler

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,20 @@ router.get("/register",(req,res) => {
     })
 })
 // post login
-router.post("/login", (req,res) => {
-    console.log(req.body);
+router.post("/login", async(req,res) => {
+    const existUser = await User.findOne({email: req.body.email})
+    if(!existUser){
+        console.log("User not found")
+        return 
+    }
+
+    const isPasswordEqual = await bcrypt.compare(req.body.password,existUser.password)
+    if(!isPasswordEqual) {
+        console.log("Password wrong")
+        return 
+    }
+    
+    console.log(existUser);
     res.redirect("/")
 })
 // post register
